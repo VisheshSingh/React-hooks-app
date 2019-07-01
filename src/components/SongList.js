781cf44755d5d88d4d429d140eedc3ec626a28aa
singhv <vishesh.singh@biworldwide.com>
2019-06-30 22:26:14 -0500

    useEffect hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid';
 import NewSong from './NewSong';
 
@@ -18,9 +18,19 @@ function SongList() {
     }
   ]);
 
+  const [age, setAge] = useState(20);
+
   const addSong = title => {
     setSong([...songs, { title: title, id: uuid.v4() }]);
   };
+
+  useEffect(() => {
+    console.log('useEffect hook for songs ran ', songs);
+  }, [songs]);
+
+  useEffect(() => {
+    console.log('useEffect hook for age ran ', age);
+  }, [age]);
 
   return (
     <div className="song-list">
@@ -28,6 +38,7 @@ function SongList() {
         <li key={song.id}>{song.title}</li>
       ))}
       <NewSong addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 }
